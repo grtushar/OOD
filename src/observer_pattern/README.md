4628b1b6b3d401daab0420f6c2622f5bdd6c6380
grushar <md.tushar_91@yahoo.com>
2019-10-08 21:30:51 +0600

    :boom: add observer design pattern with documentation

---
+++ src/observer_pattern/README.md
@@ -0,0 +1,1 @@
+![Observer pattern structure](https://github.com/shadhin/OOD/blob/master/resource/images/observer_pattern_structure.png "")
---
+++ src/observer_pattern/SubscriberOne.java
@@ -0,0 +1,8 @@
+package observer_pattern;
+
+public class SubscriberOne implements Observer {
+    @Override
+    public void update() {
+        
+    }
+}
